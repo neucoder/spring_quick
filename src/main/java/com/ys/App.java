c33355a05a2f74b44039bbfd31e0e14ce7f79226
ys <neucoder@163.com>
2022-09-12 00:48:32 +0800

    bean 的配置 单例 多例对象

--- src/main/java/com/ys/App.java
+++ src/main/java/com/ys/App.java
@@ -1,5 +1,7 @@
 package com.ys;
 
+import com.ys.dao.BookDao;
+import com.ys.dao.impl.BookDaoImpl;
 import com.ys.service.BookService;
 import com.ys.service.impl.BookServiceImpl;
 import javafx.application.Application;
@@ -10,8 +12,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        BookService bookService =(BookService) ctx.getBean("bookService");
-        bookService.save();
-
+        BookDao bookDao1 = (BookDao) ctx.getBean("bookDao");
+        BookDao bookDao2 = (BookDao) ctx.getBean("bookDao");
+        System.out.println(bookDao1);
+        System.out.println(bookDao2);
+        BookService bookService1 = (BookService) ctx.getBean("bookService");
+        BookService bookService2 = (BookService) ctx.getBean("bookService");
+        System.out.println(bookService1);
+        System.out.println(bookService2);
     }
 }
