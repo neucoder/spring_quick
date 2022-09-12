b47a315bc4605bb09dd40a6bc6c0014c53910c6b
ys <neucoder@163.com>
2022-09-12 20:55:30 +0800

    spring整合junit

---
+++ src/test/java/AccountServiceTest.java
@@ -0,0 +1,27 @@
+import com.ys.config.SpringConfig;
+import com.ys.service.AccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+//设置类运行器
+@RunWith(SpringJUnit4ClassRunner.class)
+//设置Spring环境对应的配置类
+@ContextConfiguration(classes = {SpringConfig.class}) //加载配置类
+//@ContextConfiguration(locations={"classpath:applicationContext.xml"})//加载配置文件
+public class AccountServiceTest {
+    //支持自动装配注入bean
+    @Autowired
+    private AccountService accountService;
+    @Test
+    public void testFindById(){
+        System.out.println(accountService.findById(1));
+
+    }
+    @Test
+    public void testFindAll(){
+        System.out.println(accountService.findAll());
+    }
+}
