620782435ad34fc69d3955010ff906c999287eea
ys <neucoder@163.com>
2022-11-08 21:56:29 +0800

    add .gitgnore

--- src/test/java/com/ys/service/AccountServiceTest.java
+++ src/test/java/com/ys/service/AccountServiceTest.java
@@ -1,6 +1,7 @@
 package com.ys.service;
 
 
+import com.ys.config.JdbcConfig;
 import com.ys.config.SpringConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +10,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SpringConfig.class)
@@ -17,9 +19,22 @@ public class AccountServiceTest {
     @Autowired
     private AccountService accountService;
 
+    @Autowired
+    private JdbcConfig jdbcConfig;
+
     @Test
     public void testTransfer() throws IOException {
         accountService.transfer("Tom","Jerry",100D);
     }
+
+    @Test
+    public void testJdbcConfig() throws NoSuchFieldException, IllegalAccessException {
+        Field driver = JdbcConfig.class.getDeclaredField("userName");
+        driver.setAccessible(true);
+        String driverVar = (String) driver.get(jdbcConfig);
+        System.out.println(driverVar);
+
+    }
+
 
 }
